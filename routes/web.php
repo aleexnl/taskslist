e53b20030b37338a30205d5665ab12d353b0d0bd
aleexnl <alex.nieto0027@gmail.com>
2021-02-10 19:07:19 +0100

    add: routes for categories

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Category;
 use App\Models\Task;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
@@ -26,6 +27,14 @@ Route::get('/', function () {
     ]);
 });
 
+Route::get('/categories', function () {
+    $categories = Category::orderBy('created_at', 'asc')->get();
+
+    return view('categories', [
+        'categories' => $categories
+    ]);
+});
+
 /**
  * Add New Task
  */
@@ -48,10 +57,40 @@ Route::post('/task', function (Request $request) {
 });
 
 /**
+ * Add New Category
+ */
+Route::post('/category', function (Request $request) {
+    $validator = Validator::make($request->all(), [
+        'name' => 'required|max:255',
+    ]);
+
+    if ($validator->fails()) {
+        return redirect('/')
+            ->withInput()
+            ->withErrors($validator);
+    }
+
+    $category = new Category;
+    $category->name = $request->name;
+    $category->save();
+
+    return redirect('/categories');
+});
+
+/**
  * Delete Task
  */
 Route::delete('/task/{id}', function ($id) {
     Task::findOrFail($id)->delete();
 
     return redirect('/');
+});
+
+/**
+ * Delete Category
+ */
+Route::delete('/category/{id}', function ($id) {
+    Category::findOrFail($id)->delete();
+
+    return redirect('/categories');
 });
