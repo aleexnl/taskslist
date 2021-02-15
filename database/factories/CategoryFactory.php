2a9ee3a3b522c4ae6d3bc625b0a11e7a1a118705
aleexnl <alex.nieto0027@gmail.com>
2021-02-15 13:23:16 +0100

    add: added Category and Task factory

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -4,26 +4,18 @@ namespace Database\Factories;
 
 use App\Models\Category;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
 
 class CategoryFactory extends Factory
 {
-    /**
-     * The name of the factory's corresponding model.
-     *
-     * @var string
-     */
     protected $model = Category::class;
-
-    /**
-     * Define the model's default state.
-     *
-     * @return array
-     */
     public function definition()
     {
         return [
-            //
-            "name" => $this->faker->word
+            'name' => $this->faker->name,
+            'parent' => Category::all()->random()->id,
+            'created_at' => now(),
+            'updated_at' => now()
         ];
     }
 }
