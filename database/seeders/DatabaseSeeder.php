1ef341419f17b7bc5223f6ecedfa8503a04e2323
aleexnl <alex.nieto0027@gmail.com>
2021-02-15 13:28:02 +0100

    refactor: updated seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,18 +3,22 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeders.
      *
      * @return void
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-
-    	$this->call([CatSeeder::class]);
+        $this->call([
+            TaskSeeder::class,
+            CategorySeeder::class,
+        ]);
     }
 }
