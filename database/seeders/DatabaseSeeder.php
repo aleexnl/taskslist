a55c4a2c385c0247b3f5186103f19f1ffe01ff5f
aleexnl <alex.nieto0027@gmail.com>
2021-02-15 13:29:13 +0100

    refactor: updated seeder call

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,8 +17,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            TaskSeeder::class,
             CategorySeeder::class,
+            TaskSeeder::class,
         ]);
     }
 }
