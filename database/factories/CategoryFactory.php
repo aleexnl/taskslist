78956b9db9c5012aa57183b1e98dee254c9a541f
aleexnl <alex.nieto0027@gmail.com>
2021-02-15 13:27:43 +0100

    fix: category factory random

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -13,7 +13,6 @@ class CategoryFactory extends Factory
     {
         return [
             'name' => $this->faker->name,
-            'parent' => Category::all()->random()->id,
             'created_at' => now(),
             'updated_at' => now()
         ];
