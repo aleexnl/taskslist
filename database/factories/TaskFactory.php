bdb27475650c827ff7e292d227a000c38bbfbac4
aleexnl <alex.nieto0027@gmail.com>
2021-02-16 17:01:46 +0100

    refactor: used faker lorem insead of name generator

--- database/factories/TaskFactory.php
+++ database/factories/TaskFactory.php
@@ -23,7 +23,7 @@ class TaskFactory extends Factory
     public function definition()
     {
         return [
-            'name' => $this->faker->name,
+            'name' => $this->faker->sentence,
             'category_id' => Category::all()->random()->id
         ];
     }
