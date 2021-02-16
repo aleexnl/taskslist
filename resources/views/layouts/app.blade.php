8178f467eb475e3217d1bbcd7df8599bdbbbbc22
aleexnl <alex.nieto0027@gmail.com>
2021-02-16 17:08:43 +0100

    style: added bottom margin in navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,7 +9,7 @@
 
 <body>
   <div class="">
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <nav class="navbar navbar-expand-lg navbar-light bg-light mb-2">
           <div class="container-fluid">
             <a class="navbar-brand" href="/">Tasklist</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
